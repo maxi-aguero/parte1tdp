26f380f0872fbdeb9916747e71617ad1e38d8250
maxi-aguero <maximilianoaguero@aol.com>
2020-09-24 10:53:50 -0300

    commit agregar

---
+++ testAutor.java
@@ -0,0 +1,17 @@
+
+public class testAutor {
+
+	public static void main(String[] args) {
+		
+		
+		Autor autor1 = new Autor("Roald","Dahl", 1916);
+		
+				
+		Autor autor2 = new Autor("Dante", "Alighieri",1300);
+		
+		
+		autor2.setAnio(1265);
+
+	}
+
+}
