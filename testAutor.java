72dac5c1e69f1c4b38824117483f3f58283aaaad
maxi-aguero <maximilianoaguero@aol.com>
2020-09-24 11:04:22 -0300

    commit modificar

--- testAutor.java
+++ testAutor.java
@@ -4,7 +4,7 @@ public class testAutor {
 	public static void main(String[] args) {
 		
 		
-		Autor autor1 = new Autor("Roald","Dahl", 1916);
+		Autor autor1 = new Autor("H.G","Wells", 1916);
 		
 				
 		Autor autor2 = new Autor("Dante", "Alighieri",1300);
